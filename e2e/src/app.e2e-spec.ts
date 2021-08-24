32fe6865802264ffc4dde7fd4443e8275bb8d1b9
Prabakaran Kuppusamy <praba@prabakarans-mbp.home>
2021-08-24 22:08:14 +0100

    added e2e scenarios

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,9 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('pokemon app is running!');
+    expect(await page.getTitleText()).toEqual(
+      'Place sticky footer content here.'
+    );
   });
 
   afterEach(async () => {
@@ -20,6 +22,76 @@ describe('workspace-project App', () => {
       jasmine.objectContaining({
         level: logging.Level.SEVERE,
       } as logging.Entry)
+    );
+  });
+
+  it('should display the menu options to navigate', async () => {
+    await page.navigateTo();
+
+    const actual = await page.getTextByClass('.navbar-start .navbar-item');
+
+    expect(actual).toEqual('Home');
+  });
+
+  it('should navigate to home page', async () => {
+    await page.navigateTo();
+
+    await page.clickLink('Home');
+
+    expect(browser.getCurrentUrl()).toContain('home');
+    expect(await page.getTextByClass('.title')).toEqual('Home');
+  });
+
+  it('should navigate to pokemons page', async () => {
+    await page.navigateTo();
+
+    await page.clickLink('Pokemons');
+
+    expect(browser.getCurrentUrl()).toContain('pokemons');
+    expect(await page.getTextByClass('.title')).toEqual('List of pokemons');
+  });
+
+  it('should show the first 20 pokemons', async () => {
+    await page.navigateTo();
+
+    await page.clickLink('Pokemons');
+
+    expect(await page.getTextByClassAndText('.subtitle', '#020')).toEqual(
+      '#020'
+    );
+  });
+
+  it('should show the next 20 pokemons', async () => {
+    await page.navigateTo();
+
+    await page.clickLink('Pokemons');
+    await page.clickButton('Load more');
+
+    expect(await page.getTextByClassAndText('.subtitle', '#040')).toEqual(
+      '#040'
+    );
+  });
+
+  it('should show the search pokemons', async () => {
+    await page.navigateTo();
+
+    await page.clickLink('Pokemons');
+    await page.enterInput('.input', '120');
+
+    expect(await page.getTextByClassAndText('.title', 'staryu')).toEqual(
+      'staryu #120'
+    );
+  });
+
+  it('should show pokemon detail', async () => {
+    await page.navigateTo();
+
+    await page.clickLink('Pokemons');
+    await page.enterInput('.input', '120');
+    await page.clickLink('Detail');
+
+    expect(await page.getTextByClassAndText('.title', 'STARYU')).toEqual(
+      'STARYU #120'
     );
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -8,4 +8,27 @@ export class AppPage {
   async getTitleText(): Promise<string> {
     return element(by.css('app-root .content span')).getText();
   }
+
+  async getTextByClass(className: string): Promise<string> {
+    return element(by.css(className)).getText();
+  }
+
+  async clickLink(linkText: string): Promise<void> {
+    return element(by.partialLinkText(linkText)).click();
+  }
+
+  async clickButton(btnText: string): Promise<void> {
+    return element(by.partialButtonText(btnText)).click();
+  }
+
+  async enterInput(className: string, value: string): Promise<void> {
+    return element(by.css(className)).sendKeys(value);
+  }
+
+  async getTextByClassAndText(
+    className: string,
+    linkText: string
+  ): Promise<string> {
+    return element(by.cssContainingText(className, linkText)).getText();
+  }
 }
